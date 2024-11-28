6c7f09e2e9fcf559bdf7220796a7abaab712b5cd
Nicolau3A <00001097168682sp@al.educacao.sp.gov.br>
2024-11-28 00:58:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Matematica',
+    'Qual é soma dessa equação 584+936x58÷5?',
+    '11.441,6'
+)
+
+criaCartao(
+    'Historia',
+    'Quando-se inciou a guerra fria?',
+    '12 de março de 194'
+)
+
+criaCartao(
+    'Educação Fisica',
+    'Em que o atletismo ajuda?',
+    'desenvolvimento de habilidades motoras'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual pais tem mais recursos naturais?',
+    'Rússia'
+) 
+
+criaCartao(
+    'Português',
+    'Qual é a importancia do Zumbi do Palmares?',
+    'Foi um dos principais representantes da resistência negra à escravidão na época do Brasil Colonial'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual substancia mais perigosa?',
+    'toxina botulínica'
+)
